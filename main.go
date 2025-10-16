c026b068b811e533af1ca22de495f172fa1205e2
chun <lamchunyu00@gmail.com>
2025-10-16 23:05:37 +0800

    feat: add window sizing

--- main.go
+++ main.go
@@ -5,18 +5,32 @@ import (
 
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+const (
+	MinTerminalWidth = 40 // Minimum size of the terminal for omnia to function
+	MaxBoxWidth      = 80 // Maximum size of the Launcher input box
 )
 
 type model struct {
 	launcherInput textinput.Model
+	windowWidth   int
+	windowHeight  int
 }
 
+// Helper Functions
+func getBoxWidth(windowWidth int) int {
+	// NOTE: -4 to account for padding and border on both sides
+	return min(windowWidth-4, MaxBoxWidth)
+}
+
+// Bubble Tea Model
 func initialModel() model {
 	ti := textinput.New()
 	ti.Placeholder = "Type something to launch..."
 	ti.Focus()
-	ti.CharLimit = 256
-	ti.Width = 50
+	ti.CharLimit = 512
 	return model{
 		launcherInput: ti,
 	}
@@ -29,6 +43,11 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.windowWidth = msg.Width
+		m.windowHeight = msg.Height
+		boxWidth := getBoxWidth(m.windowWidth)
+		m.launcherInput.Width = boxWidth
 	case tea.KeyMsg:
 		switch msg.Type {
 		case tea.KeyEnter, tea.KeyCtrlC, tea.KeyEsc:
@@ -41,7 +60,15 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	return m.launcherInput.View()
+	boxWidth := getBoxWidth(m.windowWidth)
+	launcherBox := lipgloss.NewStyle().
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("63")).
+		Padding(1).
+		Width(boxWidth)
+
+	content := m.launcherInput.View()
+	return lipgloss.Place(m.windowWidth, m.windowHeight, lipgloss.Center, lipgloss.Center, launcherBox.Render(content))
 }
 
 func main() {
