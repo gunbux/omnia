699996e66ac0c5b542e9c6301f7c29563506515e
chun <lamchunyu00@gmail.com>
2025-10-21 21:53:55 +0800

    feat(completion): added better rendering for app completions

--- app_completion.go
+++ app_completion.go
@@ -1,8 +1,8 @@
 package main
 
+// TODO: Put all the different type of completions in a folder
+
 import (
-	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -10,7 +10,6 @@ import (
 	"github.com/adrg/xdg"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"gopkg.in/ini.v1"
 )
 
@@ -25,59 +24,7 @@ func (d DesktopEntry) FilterValue() string { return d.id }
 func (d DesktopEntry) Title() string       { return d.id }
 func (d DesktopEntry) Description() string { return d.description }
 
-// Type defining how the app completion list should render
-type appCompletionDelegate struct {
-	isCompletionFocused bool
-}
-
-func (acd appCompletionDelegate) Height() int                             { return 1 }
-func (acd appCompletionDelegate) Spacing() int                            { return 0 }
-func (acd appCompletionDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
-func (acd appCompletionDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	desktopEntry, ok := listItem.(DesktopEntry)
-	if !ok {
-		return
-	}
-
-	normalStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("250")).
-		PaddingLeft(1)
-
-	selectedStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("15")).
-		Background(lipgloss.Color("63")).
-		Bold(true).
-		PaddingLeft(1)
-
-	descriptionStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("240")).
-		Italic(true)
-
-	prefix := " "
-	var renderedContent string
-
-	if desktopEntry.description != "" {
-		content := fmt.Sprintf("%s - %s", desktopEntry.id, desktopEntry.description)
-		if m.Index() == index && acd.isCompletionFocused {
-			prefix = ">"
-			renderedContent = selectedStyle.Render(fmt.Sprintf("%s %s", prefix, content))
-		} else {
-			nameDesc := fmt.Sprintf("%s - ", desktopEntry.id)
-			styledName := normalStyle.Render(fmt.Sprintf("%s %s", prefix, nameDesc))
-			styledDesc := descriptionStyle.Render(desktopEntry.description)
-			renderedContent = styledName + styledDesc
-		}
-	} else {
-		if m.Index() == index && acd.isCompletionFocused {
-			prefix = ">"
-			renderedContent = selectedStyle.Render(fmt.Sprintf("%s %s", prefix, desktopEntry.id))
-		} else {
-			renderedContent = normalStyle.Render(fmt.Sprintf("%s %s", prefix, desktopEntry.id))
-		}
-	}
-
-	fmt.Fprint(w, renderedContent)
-}
+// TODO: App completions get should be initial, then we just search through this, using filtervalue?
 
 // Get autocompletions based on applications
 func getAppCompletions() tea.Msg {
